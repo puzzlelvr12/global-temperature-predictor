ccc9c617cda06f8317cb3d6aa2d62cf3cf0fa36c
puzzlelvr12 <marie.0526h@gmail.com>
2024-11-10 14:36:28 -0600

    Added plotting style configuration

--- tempp_predictor.py
+++ tempp_predictor.py
@@ -4,3 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
+
+#Set plotting style using built in matplotlib style
+plt.style.use('default')#
+plt.rcParams['figure.figsize'] = [12, 6]
+plt.rcParams['font.size'] = 12
+plt.rcParams['axes.grid'] = True
+plt.rcParams['grid.alpha'] = 0.3
+
